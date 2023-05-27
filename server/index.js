81b51da44984fdaec39aae614a74a59ba703bcc6
Arefin Aareef <arefin.aareef@gmail.com>
2023-05-27 22:11:26 +0600

    jwt token send, verify, decode

--- server/index.js
+++ server/index.js
@@ -22,6 +22,24 @@ const client = new MongoClient(uri, {
   }
 });
 
+const verifyJWT = (req, res, next) => {
+  console.log('hitting verify jwt');
+  console.log(req.headers.authorization);
+  const authorization = req.headers.authorization
+  if(!authorization) {
+    return res.status(401).send({error: true, message: 'unauthorized access'})
+  }
+  const token = authorization.split(' ')[1];
+  console.log(token);
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+    if(error){
+      res.status(403).send({error: true, message: "unauthorize access"})
+    }
+    req.decoded = decoded;
+    next()
+  })
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -61,8 +79,7 @@ async function run() {
 
     // bookings
 
-    app.get('/bookings', async(req, res) => {
-      console.log(req.query.email);
+    app.get('/bookings', verifyJWT, async(req, res) => {
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email }
