71e229cfe7086ed8d3eb996c0e33bd4c1ba89e08
Arefin Aareef <arefin.aareef@gmail.com>
2023-05-28 15:48:12 +0600

    jwt token verified

--- client/src/pages/Bookings/Bookings.jsx
+++ client/src/pages/Bookings/Bookings.jsx
@@ -2,11 +2,12 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
 import BookingRow from "./BookingRow";
+import { useNavigate } from 'react-router-dom';
 
 const Bookings = () => {
   const { user } = useContext(AuthContext);
-
   const [bookings, setBookings] = useState([]);
+  const navigate = useNavigate()
 
   const url = `http://localhost:5000/bookings?email=${user.email}`;
   useEffect(() => {
@@ -17,8 +18,15 @@ const Bookings = () => {
       }
     })
       .then((res) => res.json())
-      .then((data) => setBookings(data));
-  }, [url]);
+      .then((data) => {
+        if(!data.error) {
+          setBookings(data)
+        }
+        else {
+          navigate('/')
+        }
+      });
+  }, [url, navigate]);
 
   const handleDelete = id => {
     const proceed = confirm('Want to delete?')
--- server/index.js
+++ server/index.js
@@ -80,6 +80,13 @@ async function run() {
     // bookings
 
     app.get('/bookings', verifyJWT, async(req, res) => {
+      const decoded = req.decoded;
+      console.log('come back after decoded', decoded);
+
+      if(decoded.email !== req.query.email) {
+        return res.status(403).send({error: 1, message: "forbidden access"})
+      }
+
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email }
